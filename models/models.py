9886ecd097ff434bc120cff6f256655900adb973
skdariyasafru <sk.dariyasafru@gmail.com>
2026-02-16 19:05:23 +0530

    Add Product and Order models
    Added Product and Order models to manage products and orders.

--- models/models.py
+++ models/models.py
@@ -1,16 +1,30 @@
 
 from db import db
 from flask_login import UserMixin
+class Product(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    image = db.Column(db.String(500))
+    description = db.Column(db.Text)
+
+
+class Order(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(200))
+    product_name = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    quantity = db.Column(db.Integer)
+    total = db.Column(db.Float)
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
 
-class Product(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200))
-    price = db.Column(db.Float)
+
 
 class Cart(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -18,6 +32,4 @@ class Cart(db.Model):
     product_id = db.Column(db.Integer)
     quantity = db.Column(db.Integer, default=1)
 
-class Order(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100))
+
