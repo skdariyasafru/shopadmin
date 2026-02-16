86567e274205a71e9a700a7a318a7da4c38a1f93
skdariyasafru <sk.dariyasafru@gmail.com>
2026-02-16 19:52:34 +0530

    Update configuration settings in config.py
    Updated SECRET_KEY and DATABASE_URL configurations. Added SESSION_COOKIE_SECURE and SESSION_COOKIE_SAMESITE settings.

--- config.py
+++ config.py
@@ -1,11 +1,15 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "secret")
+
+    SECRET_KEY = os.getenv("SECRET_KEY", "super-secret-key")
 
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL",
-        "sqlite:///site.db"
+        "postgresql://username:password@host:5432/dbname"
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    SESSION_COOKIE_SECURE = True
+    SESSION_COOKIE_SAMESITE = "Lax"
